8457fc2f3a1994e93567b9b21f3f3e696ca15c85
Andrey <loockerup@gmail.com>
2023-06-23 14:43:58 +0700

    Точно навёл порядок в папках

---
+++ dz/dz_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7.Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает **последнюю** цифру этого числа.
+    
+//     456 -> 6
+    
+//     782 -> 2
+    
+//     918 -> 8
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+int value = int.Parse(Console.ReadLine());
+
+int itog = value % 10;
+Console.WriteLine($"{value} -> " + itog);
